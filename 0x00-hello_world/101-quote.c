8cd3f6638d2711a4aa447f515da522139b717a28
mariammahrous23 <mariammahrous23@gmail.com>
2023-08-17 06:06:31 -0700

    8.0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - main block
+ * Return: 0
+ */
+int main(void)
+{
+	char arr[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	write(1.arr.59);
+	return (1);
+}
