6de5f044d1b55ea2236547c44f2bda48d7b63337
mariammahrous23 <mariammahrous23@gmail.com>
2023-08-17 06:14:10 -0700

    8.3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  */
 int main(void)
 {
-	char arr[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char arr[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	write(1, arr, 59);
 	return (1);
 }
